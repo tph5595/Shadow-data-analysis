42e1a18feb869957f45d0e62a9524abc8277a462
Taylor Henderson <tph5595@verizon.net>
2024-09-23 16:20:19 -0400

    format ready for charting

--- DoT/code/metrics/best.py
+++ DoT/code/metrics/best.py
@@ -15,4 +15,19 @@ for experiment, data in d.items():
             if scope not in best or best[scope] < recall_at_1:
                 best[scope] = recall_at_1
                 output[experiment][scope] = m
-print(json.dumps(output))
+
+with open('best.json', 'w') as f:
+    json.dump(output, f)
+
+# format for graph needs
+for experiment, data in output.items():
+    with open("{}.csv".format(experiment), "w") as f:
+        f.write("Missing title")
+        for scope, data in data.items():
+            f.write("{},{},{},{},{}".format(
+                data[0]['1'][2]['f1'],
+                data[0]['2'][2]['f1'],
+                data[0]['4'][2]['f1'],
+                data[0]['8'][2]['f1'],
+                scope
+                ))
